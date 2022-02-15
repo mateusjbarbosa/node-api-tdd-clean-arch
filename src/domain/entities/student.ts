0a956e9dd76b182f3b61b085bcaacf4a6070cd46
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 15:23:40 -0300

    feat: create student entity domain

---
+++ src/domain/entities/student.ts
@@ -0,0 +1,20 @@
+import { Entity } from "../../core/domain/Entity";
+
+type StudentProps = {
+  email: string
+  name: string
+}
+
+export class Student extends Entity<StudentProps> {
+  private constructor(props: StudentProps, id?: string) {
+    super(props, id)
+  }
+
+  get id() { return this._id }
+
+  static create(props: StudentProps, id?: string) {
+    const student = new Student(props, id)
+
+    return student
+  }
+}
